c120cab50aea991485b76466f8ee077bb31d5f33
Sergey <kuzmenkos1986@gmail.com>
2015-12-23 16:52:00 +0200

    work finish

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -1,5 +1,9 @@
 $(function(){
 
+    $('.swiper-container').each(function () {
+        Slider($(this));
+    });
+
     $('.menu__btn').on({
         'click':function(){
             var curElem = $(this).parent();
@@ -12,4 +16,36 @@ $(function(){
 
         }
     });
+
 } );
+
+var Slider = function (obj) {
+
+    //private properties
+    var _self = this,
+        _paginator = obj.find($('.slider__icon')),
+        _obj = obj;
+
+    //private methods
+    var _addEvents = function () {
+
+        },
+        _init = function () {
+            _addEvents();
+        };
+    if (_obj.hasClass('slider__wrap')) {
+        var _swiperPromo = new Swiper(_obj, {
+            slidesPerView: 1,
+            autoplay: 15000,
+            pagination: _paginator,
+            loop: true,
+            paginationClickable: true
+        });
+    }
+
+    //public properties
+
+    //public methods
+
+    _init();
+};
